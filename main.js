dc6f286f48f7d9a5df6f4af5caae317798d42356
Kishoiyan-Charity1 <kishoiyancharity@gmail.com>
2022-07-24 12:13:56 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,72 @@
+class Mkulima {
+    constructor() {
+      this.farms = [];
+      this.products = [];
+      this.orders = [];
+      this.addFarm =(farmId,name,farmer,phone,address) => {
+      this.farms.push({farmId,name,farmer,phone,address})
+    }
+
+      this.removeFarm = (farmId)=>{
+      let specific = this.farms.find(item=>item.farmId===farmId);
+      let farm_index = this.farms.indexOf(specific);
+      this.farms.splice(farm_index, 1);
+    }
+
+      this.updateFarm=function(farmId,name,farmer,phone,address){
+      let specificFarm =this.farms.find(item=>item.farmId===farmId)
+      specificFarm.farmId=farmId;
+      specificFarm.name=name;
+      specificFarm.farmer=farmer;
+      specificFarm.phone=phone;
+      specificFarm.address=address;
+    }
+
+    this.getFarm =(farmId)=>{
+    console.log(this.farms.find(object=>object.Id===farmId))
+    }
+
+    this.addProduct=(productId, name, price)=>{
+    this.product.push({productId,name,price})
+    }
+
+    this.removeProduct = (productId) => {
+    let specific= this.product.find(item=> item.productId===productId);
+    let productIndex = this.product.indexOf(specific);
+    this.product.splice(productIndex, 1);
+    }
+
+    this.updateProduct = function(productId,name,price){
+    let specificProduct=this.product.find(item=> item.productId===productId)
+    specificProduct.productId=productId;
+    specificProduct.name=name;
+    specificProduct.price= price;
+    }
+
+    this.getProduct=(Id)=>{
+    console.log(this.product.find(object=>object.productId===Id))
+    }
+
+    this.printProducts= () =>{
+    console.log()
+    }
+
+    this.calculateOrderCost = (productId,quantity)=>{
+    let product=this.product.find(object=>object.productId===productId);
+    console.log(`${quantity} ${product.name} for ksh ${quantity*product.price}`);
+    }
+
+    let farmers=new Mkulima();
+    farmers.addFarm("222","maize","Aley","072233744","616 Korongo");
+    farmers.addFarm("99","peas","Ken","07274633","1002 Narok");
+    farmers.addFarm("890","beans","Abu","07444483","663 Nairobi");
+    console.log(farmers.farms);
+
+    farmers.removeFarm("222");
+    console.log()
+
+    }
+  
+  
+  }
+  
